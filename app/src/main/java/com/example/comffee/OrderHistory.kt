9e8da0f4a39961762ca38cae1067f64e9e565e30
edwanlimanjaya <wanlim0810@gmail.com>
2023-04-19 20:29:45 +0700

    membuat layout history, order history dan detail history

---
+++ app/src/main/java/com/example/comffee/OrderHistory.kt
@@ -0,0 +1,11 @@
+package com.example.comffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_history)
+    }
+}
