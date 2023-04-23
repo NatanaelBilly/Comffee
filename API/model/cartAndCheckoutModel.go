96ee03cea8e645de062b75280ed878bb02ac1e17
NatanaelBilly <natanaelbilly21@gmail.com>
2023-04-23 09:55:29 +0700

    update model cart and checkout

--- API/model/cartAndCheckoutModel.go
+++ API/model/cartAndCheckoutModel.go
@@ -1,6 +1,6 @@
 package model
 
-type item struct {
+type Item struct {
 	Item_id     int    `json:"item_id"`
 	Item_name   string `json:"item_name"`
 	Description string `json:"description"`
@@ -8,14 +8,32 @@ type item struct {
 	Picture     string `json:"picture"`
 }
 
-type cart struct {
+type Cart struct {
 	Cart_id int `json:"cart_id"`
 	User_id int `json:"user_id"`
 }
 
-type cartitem struct {
+type Cartitem struct {
 	Cartitem_id int `json:"cartitem_id"`
 	Cart_id     int `json:"cart_id"`
 	Item_id     int `json:"item_id"`
 	Quantity    int `json:"quantity"`
+}
+
+type ItemResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []Item `json:"data"`
+}
+
+type CartitemResponse struct {
+	Status  int        `json:"status"`
+	Message string     `json:"message"`
+	Data    []Cartitem `json:"data"`
+}
+
+type CartResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []Cart `json:"data"`
 }
