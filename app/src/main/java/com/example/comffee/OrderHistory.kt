0abb755c3accc5529e9397bb0c5eb1afc08f1b7f
edwanlimanjaya <wanlim0810@gmail.com>
2023-04-23 17:53:28 +0700

    pindah dari history ke order history

--- app/src/main/java/com/example/comffee/OrderHistory.kt
+++ app/src/main/java/com/example/comffee/OrderHistory.kt
@@ -1,11 +1,29 @@
 package com.example.comffee
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class OrderHistory : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_history)
+
+        val topBar = findViewById<MaterialToolbar>(R.id.TopBarApp)
+
+        topBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home_menu -> {
+                    setContentView(R.layout.activity_main)
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.product_menu -> {
+                    setContentView(R.layout.activity_order_history)
+                    return@setOnMenuItemClickListener true
+                }
+                else -> return@setOnMenuItemClickListener false
+            }
+
+        }
     }
 }
