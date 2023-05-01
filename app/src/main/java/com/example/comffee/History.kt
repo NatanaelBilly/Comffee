ff1c96daaa69fd112750589ea7b46520263af68f
Rikat19 <richard.rikat19@gmail.com>
2023-05-02 00:56:19 +0700

    history reset

--- app/src/main/java/com/example/comffee/History.kt
+++ app/src/main/java/com/example/comffee/History.kt
@@ -3,23 +3,25 @@ package com.example.comffee
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ProgressBar
 import androidx.recyclerview.widget.RecyclerView
 import com.example.comffee.databinding.ActivityHistoryBinding
 import com.example.comffee.databinding.ActivityHomepageBinding
+import com.example.comffee.databinding.ActivityOrderBinding
+import com.example.comffee.databinding.ActivityProfileBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class History : AppCompatActivity() {
 
     private lateinit var binding: ActivityHistoryBinding
+
     private val auth = FirebaseAuth.getInstance()
 
-    private lateinit var adapter:NoteAdapter
-    private lateinit var noteHelper: NoteHelper
-    private lateinit var progressBar: ProgressBar
-    private lateinit var rv_notes: RecyclerView
-    private lateinit var fab_add: FloatingActionButton
+    private val firestore = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +33,25 @@ class History : AppCompatActivity() {
             val loginIntent = Intent(this, Homepage::class.java)
             startActivity(loginIntent)
         }
+
+//        getItemData()
     }
+
+//    private fun getItemData() {
+//        itemId = "bk1"
+//        val itemData = firestore.collection("items").document(itemId)
+//        itemData.get()
+//            .addOnSuccessListener {
+//
+//                println("data = $it")
+//                // set Items
+//                val item = "Nama : ${it.data?.get("nama_barang").toString()}" +
+//                        "\n Harga : ${it.data?.get("harga").toString()}"
+//                binding.tvItem.text = item
+//
+//            }
+//            .addOnFailureListener {
+//                Log.e("Firestore error!", it.message.toString())
+//            }
+//    }
 }
