4e0a21f6b578b84fcd903e8d7af39a108db3065b
Matthew Evans <85978388+MatthewEvansH@users.noreply.github.com>
2023-05-06 17:05:52 +0700

    itemList beres

--- app/src/main/java/com/example/comffee/Item.kt
+++ app/src/main/java/com/example/comffee/Item.kt
@@ -4,4 +4,5 @@ data class Item(
     var item_id : String ?= null,
     var nama_barang : String ?= null,
     var harga : Long ?= null,
+    var imagePath : String ?= null
 )
--- app/src/main/java/com/example/comffee/ItemList.kt
+++ app/src/main/java/com/example/comffee/ItemList.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ItemList : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var itemArrayList: ArrayList<Item>
     private lateinit var itemAdapter: ItemAdapter
-    private lateinit var db: FirebaseFirestore
+    private val firestore = Firebase.firestore
+    private val auth = FirebaseAuth.getInstance()
+    private val currentUser = auth.currentUser
+    val userData = firestore.collection("users").document(currentUser?.email.toString())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_list)
 
-        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView = findViewById(R.id.itemRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
@@ -34,8 +40,7 @@ class ItemList : AppCompatActivity() {
 
     private fun EventChangeListener() {
 
-        db = FirebaseFirestore.getInstance()
-        db.collection("items").orderBy("nama_barang", Query.Direction.ASCENDING)
+        firestore.collection("items").orderBy("nama_barang", Query.Direction.ASCENDING)
             .addSnapshotListener(object : EventListener<QuerySnapshot> {
                 override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
                     if (error != null) {
